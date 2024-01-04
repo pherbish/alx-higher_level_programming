14d27085432b3af357ac68eabab5b8837f4e9da5
pherbish <pherbishorwa@gmail.com>
2024-01-04 11:48:11 -0800

    More functions

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    arr = dir()
+    for i in range(1, len(arr):
+            if arr != ("__"):
+            print("{:s}".format(arr[i]))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from variables_load_5 import a
+    print(f"{a}")
