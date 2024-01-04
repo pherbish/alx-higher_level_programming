33ec916431aeadb8afd80995e4d43f2d2ee2db90
pherbish <pherbishorwa@gmail.com>
2024-01-04 10:13:12 -0800

    dealing with aaaaaaaaaargument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     x = len(argv) - 1
     if x < 1:
-        print("{} arguments".format(x))
+        print("{} arguments.".format(x))
     elif x == 1:
-        print("{} argument".format(x))
+        print("{} argument:".format(x))
     else:
-        print("{} arguments".format(x))
+        print("{} arguments:".format(x))
     for i in range(x):
         print("{} : {:s}".format(i + 1, argv[i + 1]))
 
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     for i in range(1, len(argv)):
         sumint += int(argv[i])
     print("{}".format(sumint))
-
