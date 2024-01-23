02cef141a87c53faff4fca7613ef7b0b92fcd97f
pherbish <pherbishorwa@gmail.com>
2024-01-23 04:15:14 -0800

    second square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining square class with private attribute"""
+class square:
+    """creates a square"""
+    def __init__(self,size):
+        """initialize the instance of square.
+        Args:
+        size(int):the argument it takes which is private"""
+        self.___seize = size
