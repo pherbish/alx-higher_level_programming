4e1e3b68e1429e3ef5a57065e3bdbe0ebccb2be1
pherbish <pherbishorwa@gmail.com>
2024-01-30 13:53:01 -0800

    avdanced task

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string():
+        return "BestSchool" * magic_string.counter + (", " if magic_string.counter > 0 else "")
+    magic_string.counter = 0
+    magic_string.counter += 1
