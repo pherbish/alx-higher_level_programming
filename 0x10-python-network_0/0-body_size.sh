fbd3a395cf6787fe759864e54db8602d6f5f2834
pherbish <pherbishorwa@gmail.com>
2024-04-26 03:27:59 -0700

    1st correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the URL and get the response headers
-response_headers=$(curl -sI "$1")
-
-# Extract the Content-Length from the response headers
-body_size=$(echo "$response_headers" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-
-# Display the size of the response body in bytes
-echo "$body_size"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
